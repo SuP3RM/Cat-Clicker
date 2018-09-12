c49f48481a31b3e801948b947cd5fb92c7ed6d0c
Mario Aranda <mario.aranda2551@gmail.com>
2018-09-11 23:13:05 -0500

    starting from stratch and guidance

--- js/app.js
+++ js/app.js
@@ -1,70 +1,28 @@
-/*
-let cat1 = document.getElementById('p_cat1');
-let cat2 = document.getElementById('p_cat2');
-let cat3 = document.getElementById('p_cat3');
-let cat4 = document.getElementById('p_cat4');
-let cat5 = document.getElementById('p_cat5');
+/* ===== MOV = Model Octopus View ===== */
 
-let count = 0;
-
-cat1.addEventListener('click', function(){
-  count += 1;
-  clicks.innerHTML = count;
-}, false);
-
-elem1.addEventListener('click', function(){
-  count += 1;
-  click.innerHTML = count;
-}, false);
+// Model, holds data.
 
 
 
+let model = {
+  class cat {
+    constructor(name, src) {
+      this.name = name;
+      this.img = "images/" + src;
+      this.count = 0;
+    }
+  };
 
-for (var cat in object) {
-  if (object.hasOwnProperty(cat)) {
+  let catChoosen = [];
+  catChoosen.push(Chewie = new Cat('Chewie', chewiecat.jpg));
+  catChoosen.push(Jetske = new Cat('Jetske', jetske.jpg));
+  catChoosen.push(Xuxa = new Cat('Xuxa', kittycat.jpg));
+  catChoosen.push(McSqwwney = new Cat('McSqwwney', McSqwwney.jpg));
+  catChoosen.push(Yana = new Cat('Yana', yana.jpg));
 
-  }
+  let currentCat = [0];
 }
 
-*/
+// View, what the user sees and interacts with.
 
-class Cat {
-  constructor(name, src) {
-    this.name = name;
-    this.img = "images/" + src;
-    this.count = 0;
-  }
-};
-
-let catChoosen = [];
-catChoosen.push(Xuxa = new Cat('Xuxa', kittycat.jpg));
-catChoosen.push(Chewie = new Cat('Chewie', chewiecat.jpg));
-catChoosen.push(Jetske = new Cat('Jetske', jetske.jpg));
-catChoosen.push(McSqwwney = new Cat('McSqwwney', McSqwwney.jpg));
-catChoosen.push(Yana = new Cat('Yana', yana.jpg));
-
-let currentCat = [0];
-
-let catName = document.querySelector('.cat-name');
-let counter = document.querySelector('.clicker');
-let catImage = document.querySelector('.cat-image');
-let buttons = document.querySelector('.cat_button');
-
-function main() {
-  counterFun();
-  catName.innerText = currentCat.name;
-  catImage.src = currentCat.img
-
-  if (main.children.style.display === 'hidden') {
-    main.children.style.display === 'hidden';
-
-  }
-};
-
-function counterFun() {
-  counter.innerText = currentCat.count;
-  currentCat.catImage.addEventListener('click', function() {
-    this.count += 1
-    click.innerHTML = count
-  })
-};
+// Octopus, ties both M&V together so they wont talk to each other.
