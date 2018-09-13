acb63579caf5702b91a9ce4bdf39a52821dfcd66
Mario Aranda <mario.aranda2551@gmail.com>
2018-09-12 22:13:08 -0500

    213

--- js/app.js
+++ js/app.js
@@ -1,31 +1,32 @@
 /* ===== MOV = Model Octopus View ===== */
 
 // Model, holds data.
+class cat {
+  constructor(name, src) {
+    this.name = name;
+    this.img = "images/" + src;
+    this.count = 0;
+  };
+}
+let catChoosen = [];
+catChoosen.push(Chewie = new cat('Chewie', 'chewiecat.jpg'));
+catChoosen.push(Jetske = new cat('Jetske', 'jetske.jpg'));
+catChoosen.push(Xuxa = new cat('Xuxa', 'kittycat.jpg'));
+catChoosen.push(McSqwwney = new cat('McSqwwney', 'McSqwwney.jpg'));
+catChoosen.push(Yana = new cat('Yana', 'yana.jpg'));
+var currentCat = null
 
-let model = {
-  class cat {
-    constructor(name, src) {
-      this.name = name;
-      this.img = "images/" + src;
-      this.count = 0;
-    }
-  },
-
-  let catChoosen = [];
-  catChoosen.push(Chewie = new Cat('Chewie', chewiecat.jpg));
-  catChoosen.push(Jetske = new Cat('Jetske', jetske.jpg));
-  catChoosen.push(Xuxa = new Cat('Xuxa', kittycat.jpg));
-  catChoosen.push(McSqwwney = new Cat('McSqwwney', McSqwwney.jpg));
-  catChoosen.push(Yana = new Cat('Yana', yana.jpg));
-
-  const currentCat = null;
+const model = {
+  currentCat
 };
 
 // Octopus, ties both M&V together so they wont talk to each other.
 
 const octopus = {
   init: function () {
-    model.currentCat = model.catChoosen[0] // det defualt cat to view
+    model.currentCat = catChoosen[0] // det defualt cat to view
+
+    // call functions to show catListView and catView on screen
     catListView.init();
     catView.init();
 
@@ -35,7 +36,18 @@ const octopus = {
     return model.currentCat;
   },
 
+  getCats: function() {
+      return model.currentCat;
+  },
 
+  setCurrentCat: function (cat) {
+    model.currentCat = catChoosen[0];
+  },
+
+  incrementCounter: function() {
+      model.currentCat.count++;
+      catView.render();
+  }
 };
 
 // View, what the user sees and interacts with.
@@ -54,20 +66,16 @@ let catView = {
     this.render();
     });
 
-
-
-
-
     // render the cat View
-    render: function () {
-      // update the DOM elements with values from the current cat
-      let Cat = octopus.getCurrentCat();
-      this.catCounter.textContent = Cat.Count;
-      this.catName.textContent = Cat.name;
-      this.catImage.src = Cat.src;
-    }
+  },
+  render: function () {
+    // update the DOM elements with values from the current cat
+    let Cat = octopus.getCurrentCat();
+    this.catCounter.textContent = Cat.Count;
+    this.catName.textContent = Cat.name;
+    this.catImage.src = Cat.src;
   }
-}
+};
 
 let catListView = {
    init: function () {
@@ -76,7 +84,36 @@ let catListView = {
      this.render();
    },
 
-   render: function () {
+   render: function() {
+       var cat, elem, i;
+       // get the cats we'll be rendering from the octopus
+       var cats = octopus.getCats();
 
-   },
-}
+       // empty the cat list
+       this.catListName.innerHTML = '';
+
+       // loop over the cats
+       for (i = 0; i < cats.length; i++) {
+           // this is the cat we're currently looping over
+           cat = cats[i];
+
+           // make a new cat list item and set its text
+           elem = document.createElement('li');
+           elem.textContent = currentCat.name;
+
+           // on click, setCurrentCat and render the catView
+           // (this uses our closure-in-a-loop trick to connect the value
+           //  of the cat variable to the click event function)
+           elem.addEventListener('click', (function(catCopy) {
+               return function() {
+                   octopus.setCurrentCat(catCopy);
+                   catView.render();
+               };
+           })(cat));
+
+           // finally, add the element to the list
+           this.catListName.appendChild(elem);
+       }
+   }
+};
+octopus.init();
